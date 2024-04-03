622bc4bde7548d93b82d2b2769fd0ad96c3248e8
ruthsarah2000 <160377609+ruthsarah2000@users.noreply.github.com>
2024-04-02 20:45:14 -0500

    Update regex_1.py
    Corrected code

--- regex_1.py
+++ regex_1.py
@@ -5,15 +5,13 @@ timestamps from 'MM-DD-YYYY' to 'YYYY-MM-DD' and anonymize any usernames mention
 import re
 
 def reformat_log(log_data):
-    
-    timestamp_pattern = r'\b(\d{2}-\d{2}-\d{4})\b'
+    timestamp_pattern = r'(\d{2})-(\d{2})-(\d{4})'
     timestamps = re.findall(timestamp_pattern, log_data)
     reformatted_log = re.sub(timestamp_pattern, r'\3-\1-\2', log_data)
     username_pattern = r'@(\w+)'
-
     anonymized_log = re.sub(username_pattern, '@username', reformatted_log)
-
     return anonymized_log
+
 log_data = "12-25-2022: @john Logged in. 01-01-2023: @jane Accessed the dashboard."
 reformatted_log = reformat_log(log_data)
 print(reformatted_log)
